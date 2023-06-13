335776d4f3571ab7e9930cfa17de6f51939c2714
Eric Weidow <85724887+Studsministern@users.noreply.github.com>
2023-06-13 21:04:39 +0200

    remove TODOs as these are now issues

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from lib.http import http_get_request
 soil_moisture_sensor = ADC(Pin(26)) # Soil moisture sensor with ADC0 pin (GP26)
 dht11 = DHT11(Pin(27))  # DHT11 Constructor with GP27 pin
 
-# TODO: Make the raspberry pi sleep instead of always being active. This both saves battery and makes the soil moisture sensor last longer.
-
 # HTTP request
 try:
     http_get_request('http://detectportal.firefox.com/')
@@ -17,9 +15,6 @@ except Exception as err:
 while True:
     time.sleep(5)
     try:
-        # TODO: Test to see what values correspond to the plant being dry and what values correspond to the plant being recently watered.
-        # In the first test read_u16() gave a value of 47000 for dry soil and 22000 while being watered. Limits of 65535 and 0 may not correspond to 0 and 100 % moisture 
-
         # Measuring soil moisture
         moisturePercent = 100 - (soil_moisture_sensor.read_u16() / 65535 * 100)
         
