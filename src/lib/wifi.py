cad61271e48cb95fea83fc99c71d1367f668057b
Eric Weidow <85724887+Studsministern@users.noreply.github.com>
2023-07-07 17:09:17 +0200

    format code with black

--- src/lib/wifi.py
+++ src/lib/wifi.py
@@ -2,34 +2,37 @@ import network
 import env
 from utime import sleep
 
+
 class WiFi:
     def connect():
-        wlan = network.WLAN(network.STA_IF) # Find WLAN
-        wlan.active(True)                   # Activate network interface
-        wlan.config(pm = 0xa11140)          # set power mode to get WiFi power-saving off (if needed)
+        wlan = network.WLAN(network.STA_IF)  # Find WLAN
+        wlan.active(True)  # Activate network interface
+        wlan.config(pm=0xA11140)  # set power mode to get WiFi power-saving off (if needed)
 
-        print('\nConnecting to network...')
+        print("\nConnecting to network...")
         wlan.connect(env.WIFI_SSID, env.WIFI_PASSWORD)
 
         # Check if it is connected otherwise wait
-        print('Waiting for connection...', end='')
+        print("Waiting for connection...", end="")
         while not wlan.isconnected() and wlan.status() >= 0:
-            print('.', end='')
+            print(".", end="")
             sleep(1)
 
         # Print the IP assigned by router
         ip = wlan.ifconfig()[0]
 
         # Check so the ip is not 0.0.0.0
-        if ip is '0.0.0.0':
-            wlan.disconnect() # Disconnect if the connection is to the wrong WiFi
-            wlan = None       # Cleanup
-            raise Exception('\nip = 0.0.0.0 - Not connected to WiFi. Check that you have a 2.4 GHz WiFi and correct credentials in env.py')
-        
-        print(f'\nConnected on {ip}\n')
-    
+        if ip is "0.0.0.0":
+            wlan.disconnect()  # Disconnect if the connection is to the wrong WiFi
+            wlan = None  # Cleanup
+            raise Exception(
+                "\nip = 0.0.0.0 - Not connected to WiFi. Check that you have a 2.4 GHz WiFi and correct credentials in env.py"
+            )
+
+        print(f"\nConnected on {ip}\n")
+
     def disconnect():
         wlan = network.WLAN(network.STA_IF)
         wlan.disconnect()
         wlan = None
-        print('Disconnected from WiFi\n')
+        print("Disconnected from WiFi\n")
--- src/main.py
+++ src/main.py
@@ -6,20 +6,24 @@ from lib.wifi import WiFi
 from lib.mqtt import MQTTClient
 
 
-
 ### SETUP ###
 # LED and sensors
-led = Pin("LED", Pin.OUT)              # The Raspberry Pi onboard LED
-dht11 = DHT11(Pin(26))                 # GP26 pin for measuring from the DHT11
-soil_moisture_power = Pin(27, Pin.OUT) # GP27 pin used to provide 3.3V VCC to the soil moisture sensor
-soil_moisture_sensor = ADC(Pin(28))    # ADC2 pin for measuring from the FC28
+led = Pin("LED", Pin.OUT)  # The Raspberry Pi onboard LED
+dht11 = DHT11(Pin(26))  # GP26 pin for measuring from the DHT11
+soil_moisture_power = Pin(27, Pin.OUT)  # GP27 pin used to provide 3.3V VCC to the soil moisture sensor
+soil_moisture_sensor = ADC(Pin(28))  # ADC2 pin for measuring from the FC28
 
 # Making sure the soil moisture sensor is off when starting the program
 soil_moisture_power.off()
 
 # Creating a MQTT client variable
-global mqttClient
-
+mqttClient = MQTTClient(
+    client_id=env.AIO_CLIENT_ID,
+    server=env.AIO_BROKER,
+    port=env.AIO_PORT,
+    user=env.AIO_USERNAME,
+    password=env.AIO_ACCESS_KEY,
+)
 
 
 ### FUNCTIONS ###
@@ -29,27 +33,32 @@ def led_blink_once(half_delay):
     led.off()
     time.sleep(half_delay)
 
+
 def measure_dht11():
     dht11.measure()
     temperature = dht11.temperature()
     humidity = dht11.humidity()
     return temperature, humidity
 
+
 def measure_fc28():
     soil_moisture_power.on()
-    time.sleep(2) # Keeping the power for the soil moisture sensor on for a few seconds so a correct measurement is received
+    # Keeping the power for the soil moisture sensor on for a few seconds so a correct measurement is received
+    time.sleep(2)
     moisturePercent = 100 - (soil_moisture_sensor.read_u16() / 65535 * 100)
     soil_moisture_power.off()
     return moisturePercent
 
+
 def mqtt_publish(mqtt_feed, data):
-    print('Publishing: {0} to {1} ... '.format(data, mqtt_feed), end='')
+    print("Publishing: {0} to {1} ... ".format(data, mqtt_feed), end="")
     try:
         mqttClient.publish(topic=mqtt_feed, msg=str(data))
-        print('DONE')
+        print("DONE")
         led_blink_once(0.25)
     except Exception as e:
-        print('FAILED', e)
+        print("FAILED", e)
+
 
 def disconnect_all():
     # Making sure the soil moisture sensor does not stay on
@@ -57,10 +66,9 @@ def disconnect_all():
 
     # Disconnect MQTT Client and WiFi
     mqttClient.disconnect()
-    print('\nDisconnected from Adafruit IO')
+    print("\nDisconnected from Adafruit IO")
 
     WiFi.disconnect()
-
 
 
 ### LOOP ###
@@ -72,22 +80,23 @@ while True:
         # Blink when the WiFi is connected
         for i in range(0, 3):
             led_blink_once(0.05)
-        
+
         # Connecting to MQTT broker
-        print(f'Begin connection with MQTT Broker :: {env.AIO_BROKER}')
-        mqttClient = MQTTClient(client_id=env.AIO_CLIENT_ID, server=env.AIO_BROKER, port=env.AIO_PORT, user=env.AIO_USERNAME, password=env.AIO_ACCESS_KEY)
+        print(f"Begin connection with MQTT Broker :: {env.AIO_BROKER}")
         mqttClient.connect()
-        print(f'Connected to MQTT Broker :: {env.AIO_BROKER}\n')
-        
+        print(f"Connected to MQTT Broker :: {env.AIO_BROKER}\n")
+
         # Blink when connected to the MQTT Broker
         for i in range(0, 3):
             led_blink_once(0.05)
-        
+
         # Measuring
         temperature, humidity = measure_dht11()
         moisturePercent = measure_fc28()
 
-        print(f'Temperature is {temperature} degrees Celcius. Humidity is {humidity}%RH. Soil moisture is {int(moisturePercent)}%')
+        print(
+            f"Temperature is {temperature} degrees Celcius. Humidity is {humidity}%RH. Soil moisture is {int(moisturePercent)}%"
+        )
 
         # Publishing
         mqtt_publish(env.AIO_FEED_TEMPERATURE, temperature)
@@ -96,15 +105,17 @@ while True:
 
         # Disconnecting and going to sleep
         disconnect_all()
-        print(f'Going to sleep for {env.AIO_PUBLISH_INTERVAL} seconds ...')
+        print(f"Going to sleep for {env.AIO_PUBLISH_INTERVAL} seconds ...")
         time.sleep(env.AIO_PUBLISH_INTERVAL)
     except KeyboardInterrupt:
         # Disconnect and clean up if the user causes a KeyboardInterrupt
         disconnect_all()
-        print('Keyboard interrupt')
+        print("Keyboard interrupt")
         break
     except Exception as e:
         # Disconnect and try connecting again if another exception is thrown
         disconnect_all()
-        print(f'Did not manage to connect to WiFi or MQTT broker. Trying again in {env.WIFI_TRY_RECONNECT_INTERVAL} seconds.')
+        print(
+            f"Did not manage to connect to WiFi or MQTT broker. Trying again in {env.WIFI_TRY_RECONNECT_INTERVAL} seconds."
+        )
         time.sleep(env.WIFI_TRY_RECONNECT_INTERVAL)
